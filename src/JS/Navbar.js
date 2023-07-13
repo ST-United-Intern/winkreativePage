7659cfdb000243ddf7bddc45e7fe00f1068141cd
NguyenThanhNhan2507 <65636315+NguyenThanhNhan2507@users.noreply.github.com>
2023-07-13 14:16:52 +0700

    Sec/home page (#5)
    * feat/Hompage
    
    * feat/fix HomePage
    
    * feat: fix conflict
    
    * feat/fix HomePage
    
    * feat: SEC animation
    
    * feat: fix conflict
    
    * feat/fix HomePage
    
    * feat: SEC animation
    
    * navbar
    
    * fix navbar

---
+++ src/JS/Navbar.js
@@ -0,0 +1,15 @@
+(function ($) {
+  var ost = 0;
+
+  $(window).scroll(function () {
+    var cOst = $(this).scrollTop();
+
+    if (cOst > ost) {
+      $("header").addClass("fixed").removeClass("default");
+    } else {
+      $("header").addClass("default").removeClass("fixed");
+    }
+
+    ost = cOst;
+  });
+})(jQuery);
